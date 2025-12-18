2d5bc43a2e8de74c2f4e6b50f8379bedb03109cf
hdcatik <hdcatik@gmail.com>
2025-12-18 20:49:57 +0500

    кря
    kria

---
+++ IlyaKnobiGrushaKriaAAAAAAAAAAAAAAAAAAAAA.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+char answer; 
+
+static void test() {
+    for (int i = 0; i < 1;) {
+        cout << "Кря! "; 
+        cin >> answer; 
+        if (answer != '0') continue;
+        else i++; 
+    }
+}
+
+
+void main()
+{
+    setlocale(LC_ALL, "Rus");
+    test();
+}
